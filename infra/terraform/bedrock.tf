0ab1a66608740b67632b37da2c2d0e13575a02b0
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2026-09-19 13:33:09 -0300

    style(terraform): format Bedrock model policy

--- infra/terraform/bedrock.tf
+++ infra/terraform/bedrock.tf
@@ -7,13 +7,13 @@ resource "aws_iam_role_policy" "ecs_task_bedrock" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Sid      = "InvokeStudyModels"
-      Effect   = "Allow"
-      Action   = ["bedrock:InvokeModel"]
-      Resource = compact([
-        var.enable_bedrock_ai ? "arn:aws:bedrock:${var.bedrock_region}::foundation-model/${var.bedrock_model_id}" : "",
-        var.enable_bedrock_embeddings ? "arn:aws:bedrock:${var.bedrock_region}::foundation-model/${var.bedrock_embedding_model_id}" : ""
-      ])
+      Sid    = "InvokeStudyModels"
+      Effect = "Allow"
+      Action = ["bedrock:InvokeModel"]
+      Resource = [
+        "arn:aws:bedrock:${var.bedrock_region}::foundation-model/${var.bedrock_model_id}",
+        "arn:aws:bedrock:${var.bedrock_region}::foundation-model/${var.bedrock_embedding_model_id}"
+      ]
     }]
   })
 }
